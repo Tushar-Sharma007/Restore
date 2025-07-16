9be20534086dd82bc3bed950214855c6946a7a37
Tushar Sharma <tusharktn40@gmail.com>
2025-07-16 18:56:49 +0530

    added routing, product details component, some styles in navbar and product details section

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -2,12 +2,16 @@ import { Fragment } from "react/jsx-runtime";
 import type { Product } from "../../app/models/product";
 
 import ProductList from "./ProductList";
+import { useEffect, useState } from "react";
 
-type Props = {
-  products : Product[];  
-}
 
-export default function Catalog({products} : Props) {
+export default function Catalog() {
+  const [products, setProducts] = useState<Product[]>([]);
+   useEffect(() => {
+      fetch('https://localhost:5001/api/products')
+        .then(response => response.json())
+        .then(data => setProducts(data))
+    },[]);
   return (
     <Fragment>
       <ProductList products={products} />
